ed3dfa4832d5ec8bd72e5bc123646d0a4cdeef68
Archie Makuwa <archie.makuwa@brafton.com>
2024-07-19 14:47:10 +0200

    Updated plugin with additional CDN and urls values that are not the orgin and are used across the CSIR websites

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# konclude-wp-optimizer
+# Konclude WordPress Optimizer
+## A plugin to enhance the security of your WordPress site by setting various security and optimizations settings.
+
+# Setup Instructions 
+### Coming soon 
+
+# Support
+## Coming soon 
--- konclude-wp-optimizer.php
+++ konclude-wp-optimizer.php
@@ -3,38 +3,60 @@
  * @package         Konclude WordPress Optimizer
  * @author          Archie Makuwa
  * @copyright       2024 Konclude (Pty) Ltd
- * @license         ??
  * 
  * Plugin Name:     Konclude WordPress Optimizer
  * Description:     A plugin to enhance the security of your WordPress site by setting various security and optimizations settings.
- * Version:         1.0.0
+ * Version:         1.0.5
  * Author:          Konclude (Archie Makuwa)
  * Author URI:      https://www.konclu.de
- * 
  */
 
-if (!defined('ABSPATH')) {
+ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
 // Add security headers based on options
 function kwo_add_security_headers() {
+    // Always append the X-Frame-Options header
+    header('X-Frame-Options: SAMEORIGIN');
+
+    // Always set the X-Content-Type-Options header
+    header('X-Content-Type-Options: nosniff');
+
+    // Always set the X-XSS-Protection header
+    header('X-XSS-Protection: 1; mode=block');
+
+    // Get options
     $options = get_option('kwo_options');
 
-    if (!empty($options['x_frame_options'])) {
-        header('X-Frame-Options: SAMEORIGIN');
+    // Base CSP
+    $csp = "default-src 'self'; ";
+    $csp .= "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.google.com https://*.gstatic.com https://*.googleapis.com https://cdn.jsdelivr.net https://www.gstatic.com/recaptcha/ blob:; ";
+    $csp .= "style-src 'self' 'unsafe-inline' https://*.google.com https://*.gstatic.com https://*.googleapis.com https://fonts.googleapis.com https://cdn.jsdelivr.net; ";
+    $csp .= "font-src 'self' data: https://*.gstatic.com https://fonts.googleapis.com https://cdn.jsdelivr.net; ";
+    $csp .= "img-src 'self' data: https://*.google.com https://*.gstatic.com https://*.googleapis.com https://secure.gravatar.com https://linkmon.sanren.ac.za https://chpc.ac.za; ";
+    $csp .= "frame-src 'self' https://www.google.com https://maps.google.com https://docs.google.com; ";
+    $csp .= "connect-src 'self' https://maps.googleapis.com; ";
+    $csp .= "worker-src 'self' blob:; "; // Allow blob URLs for workers
+
+    // Add custom domains from options
+    if (!empty($options['custom_csp_domains'])) {
+        $custom_domains = explode(',', $options['custom_csp_domains']);
+        foreach ($custom_domains as $domain) {
+            $domain = trim($domain);
+            $csp .= "script-src $domain; ";
+            $csp .= "style-src $domain; ";
+            $csp .= "font-src $domain; ";
+            $csp .= "img-src $domain; ";
+            $csp .= "frame-src $domain; ";
+            $csp .= "connect-src $domain; ";
+        }
     }
-    if (!empty($options['csp'])) {
-        header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' https://trusted.cdn.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://api.trusted.com");
-    }
+
+    header("Content-Security-Policy: $csp");
+
     if (!empty($options['hsts'])) {
         header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
-    }
-    if (!empty($options['x_content_type_options'])) {
-        header('X-Content-Type-Options: nosniff');
-    }
-    if (!empty($options['x_xss_protection'])) {
-        header('X-XSS-Protection: 1; mode=block');
     }
     if (!empty($options['referrer_policy'])) {
         header('Referrer-Policy: no-referrer-when-downgrade');
@@ -46,12 +68,13 @@ function kwo_add_security_headers() {
 add_action('send_headers', 'kwo_add_security_headers');
 
 // Disable XML-RPC to prevent brute force attacks
-if (!empty(get_option('kwo_options')['disable_xmlrpc'])) {
+$options = get_option('kwo_options');
+if (!empty($options['disable_xmlrpc'])) {
     add_filter('xmlrpc_enabled', '__return_false');
 }
 
 // Disable file editing from the admin panel
-if (!empty(get_option('kwo_options')['disable_file_editing'])) {
+if (!empty($options['disable_file_editing'])) {
     define('DISALLOW_FILE_EDIT', true);
 }
 
@@ -79,41 +102,9 @@ function kwo_settings_init() {
     );
 
     add_settings_field(
-        'x_frame_options',
-        __('X-Frame-Options', 'kwo'),
-        'kwo_x_frame_options_render',
-        'kwo',
-        'kwo_section_headers'
-    );
-
-    add_settings_field(
-        'csp',
-        __('Content-Security-Policy', 'kwo'),
-        'kwo_csp_render',
-        'kwo',
-        'kwo_section_headers'
-    );
-
-    add_settings_field(
         'hsts',
         __('HTTP Strict Transport Security (HSTS)', 'kwo'),
         'kwo_hsts_render',
-        'kwo',
-        'kwo_section_headers'
-    );
-
-    add_settings_field(
-        'x_content_type_options',
-        __('X-Content-Type-Options', 'kwo'),
-        'kwo_x_content_type_options_render',
-        'kwo',
-        'kwo_section_headers'
-    );
-
-    add_settings_field(
-        'x_xss_protection',
-        __('X-XSS-Protection', 'kwo'),
-        'kwo_x_xss_protection_render',
         'kwo',
         'kwo_section_headers'
     );
@@ -156,6 +147,14 @@ function kwo_settings_init() {
         'kwo',
         'kwo_section_other'
     );
+
+    add_settings_field(
+        'custom_csp_domains',
+        __('Custom CSP Domains', 'kwo'),
+        'kwo_custom_csp_domains_render',
+        'kwo',
+        'kwo_section_headers'
+    );
 }
 add_action('admin_init', 'kwo_settings_init');
 
@@ -167,38 +166,10 @@ function kwo_section_other_cb() {
     echo __('Additional security options.', 'kwo');
 }
 
-function kwo_x_frame_options_render() {
-    $options = get_option('kwo_options');
-    ?>
-    <input type='checkbox' name='kwo_options[x_frame_options]' <?php checked($options['x_frame_options'], 1); ?> value='1'>
-    <?php
-}
-
-function kwo_csp_render() {
-    $options = get_option('kwo_options');
-    ?>
-    <input type='checkbox' name='kwo_options[csp]' <?php checked($options['csp'], 1); ?> value='1'>
-    <?php
-}
-
 function kwo_hsts_render() {
     $options = get_option('kwo_options');
     ?>
     <input type='checkbox' name='kwo_options[hsts]' <?php checked($options['hsts'], 1); ?> value='1'>
-    <?php
-}
-
-function kwo_x_content_type_options_render() {
-    $options = get_option('kwo_options');
-    ?>
-    <input type='checkbox' name='kwo_options[x_content_type_options]' <?php checked($options['x_content_type_options'], 1); ?> value='1'>
-    <?php
-}
-
-function kwo_x_xss_protection_render() {
-    $options = get_option('kwo_options');
-    ?>
-    <input type='checkbox' name='kwo_options[x_xss_protection]' <?php checked($options['x_xss_protection'], 1); ?> value='1'>
     <?php
 }
 
@@ -227,6 +198,14 @@ function kwo_disable_file_editing_render() {
     $options = get_option('kwo_options');
     ?>
     <input type='checkbox' name='kwo_options[disable_file_editing]' <?php checked($options['disable_file_editing'], 1); ?> value='1'>
+    <?php
+}
+
+function kwo_custom_csp_domains_render() {
+    $options = get_option('kwo_options');
+    ?>
+    <textarea name='kwo_options[custom_csp_domains]' rows='5' cols='50'><?php echo esc_textarea($options['custom_csp_domains']); ?></textarea>
+    <p class="description"><?php _e('Enter additional domains to be included in the CSP directives, separated by commas.', 'kwo'); ?></p>
     <?php
 }
 
